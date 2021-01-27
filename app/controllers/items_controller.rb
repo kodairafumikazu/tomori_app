a03f048a3f12ffdc8f6939a4d845806c3d744b75
kodairafumikazu <z27d6a27afh862000@yahoo.co.jp>
2021-01-27 21:37:22 +0900

    HTML、CSSの修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    #@items = Item.all
   end
 
   def new
+    #@item = Item.all
   end
+
+  def create
+    #Item.create(item_params)
+  end
+
+  private
+  def item_params
+    #params.require(:item).permit(:name, :iamge, :text)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,10 +34,7 @@ ActiveRecord::Schema.define(version: 2021_01_26_062512) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "first_name_kana", null: false
-    t.string "last_name_kana", null: false
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
