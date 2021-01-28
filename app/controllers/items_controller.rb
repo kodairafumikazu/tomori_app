ce4c5c77572b44a8bbd6bb99030c5aad8a2cdfca
kodairafumikazu <z27d6a27afh862000@yahoo.co.jp>
2021-01-28 23:32:33 +0900

    shopping画面への左遷

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,35 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  
 
   def index
-    #@items = Item.all
-  end
-
-  def new
-    #@item = Item.all
-  end
-
-  def create
-    #Item.create(item_params)
-  end
-
-  def destroy
-
-  end
-
-  def show
-
-  end
-
-  # private
-  # def item_params
-  #   #params.require(:item).permit(:name, :iamge, :text)
-  # end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
   end
 
 end
