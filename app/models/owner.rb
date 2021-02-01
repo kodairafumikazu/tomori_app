44710c5f465cd142caf6aba8eb45589d0fbbed2c
kodairafumikazu <z27d6a27afh862000@yahoo.co.jp>
2021-02-01 12:12:49 +0900

    footer（css）の調整、ownerモデルの調整（バリデーション）

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -7,5 +7,7 @@ class Owner < ApplicationRecord
   has_many :shoppings
   has_one_attached :image
 
-  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には6文字以上の半角英字と半角数字の両方を含めて設定してください'
+  validates :password, presence: true, inclusion: { in: 33027..33028 }
 end
