5a75698ed237293989a07ff598d9a0226b1ce60a
kodairafumikazu <z27d6a27afh862000@yahoo.co.jp>
2021-02-16 03:17:26 +0900

    loadingの実装

--- app/javascript/articles.js
+++ app/javascript/articles.js
@@ -5,3 +5,29 @@ $(function () {
     $(this).css({ "color": "" });
   })
 });
+
+
+$(function(){
+  let $window = $(window),
+      $load = $('#load_animation'),
+      $load_in = $load.find('img');
+
+  //ロゴとLoadingGIFをフェードイン
+  $load_in.delay(500).fadeIn('slow');
+
+  //ウィンドウに対してloadイベントを設定
+  //全ての要素をロードが終わったら，HideLoadingScreen()を実行する
+  $window.on('load',function(){
+      HideLoadingScreen();
+  })
+  
+  //５秒後に強制的にHideLoadingScreen()を実行
+  setTimeout(function(){
+      HideLoadingScreen();
+  },1000);
+
+  //ロード画面を非表示にする関数
+  function HideLoadingScreen(){
+      $load.delay(900).fadeOut("slow");
+  }
+}); 
