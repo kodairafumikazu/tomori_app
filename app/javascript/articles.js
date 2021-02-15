e7f77e32404a54de9d1b9903260e980b25ed58ec
kodairafumikazu <z27d6a27afh862000@yahoo.co.jp>
2021-02-15 21:23:17 +0900

    画像やjQueryの調整

--- app/javascript/articles.js
+++ app/javascript/articles.js
@@ -1,7 +1,7 @@
 $(function () {
-  $(document).on("mouseover", "h1", function () {
-    $(this).css({ "color": "blue" });
-  }).on("mouseout", "h1", function () {
+  $(document).on("mouseover", "h2", function () {
+    $(this).css({ "color": "pink" });
+  }).on("mouseout", "h2", function () {
     $(this).css({ "color": "" });
   })
 });
